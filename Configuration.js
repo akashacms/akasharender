b34621ba28cdd98fabadd4e67bdf3e2899442806
David Herron <david@davidherron.com>
2016-06-01 23:05:57 -0700

    Internal documentation

--- Configuration.js
+++ Configuration.js
@@ -11,11 +11,22 @@ const Plugin = require('./Plugin');
 const log    = require('debug')('akasha:configuration');
 const error  = require('debug')('akasha:error-configuration');
 
+/**
+ * Configuration of an AkashaRender project, including the input directories,
+ * output directory, plugins, and various settings.
+ */
 module.exports = class Configuration {
     constructor() {
 
     }
 
+    /**
+     * Initialize default configuration values for anything which has not
+     * already been configured.  Some built-in defaults have been decided
+     * ahead of time.  For each configuration setting, if nothing has been
+     * declared, then the default is substituted.
+     * @returns {Configuration}
+     */
     prepare() {
 
         var stat;
@@ -86,11 +97,13 @@ module.exports = class Configuration {
         //
         this.use(require('./built-in'));
 
+        return this;
     }
 
 
     /**
      * Add a directory to the documentDirs configuration array
+     * @param {string} dir The pathname to use
      */
     addDocumentsDir(dir) {
         if (!this.documentDirs) { this.documentDirs = []; }
@@ -100,6 +113,7 @@ module.exports = class Configuration {
 
     /**
      * Add a directory to the layoutDirs configurtion array
+     * @param {string} dir The pathname to use
      */
     addLayoutsDir(dir) {
         if (!this.layoutDirs) { this.layoutDirs = []; }
@@ -109,6 +123,8 @@ module.exports = class Configuration {
 
     /**
      * Add a directory to the partialDirs configurtion array
+     * @param {string} dir The pathname to use
+     * @returns {Configuration}
      */
     addPartialsDir(dir) {
         if (!this.partialDirs) { this.partialDirs = []; }
@@ -118,6 +134,8 @@ module.exports = class Configuration {
 
     /**
      * Add a directory to the assetDirs configurtion array
+     * @param {string} dir The pathname to use
+     * @returns {Configuration}
      */
     addAssetsDir(dir) {
         if (!this.assetDirs) { this.assetDirs = []; }
@@ -127,6 +145,8 @@ module.exports = class Configuration {
 
     /**
      * Add an array of Mahabhuta functions
+     * @param {Array} mahafuncs
+     * @returns {Configuration}
      */
     addMahabhuta(mahafuncs) {
         if (!this.mahafuncs) { this.mahafuncs = []; }
@@ -134,17 +154,30 @@ module.exports = class Configuration {
         return this;
     }
 
+    /**
+     * Define the directory into which the project is rendered.
+     * @param {string} dir The pathname to use
+     * @returns {Configuration}
+     */
     setRenderDestination(dir) {
         this.renderTo = dir;
         return this;
     }
 
+    /** Fetch the declared destination for rendering the project. */
     get renderDestination() { return this.renderTo; }
 
     /* TODO:
 
     addMetadataObject - object */
 
+    /**
+     * Add a value to the project metadata.  The metadata is combined with
+     * the document metadata and used during rendering.
+     * @param {string} index The key to store the value.
+     * @param value The value to store in the metadata.
+     * @returns {Configuration}
+     */
     addMetadata(index, value) {
         if (typeof this.metadata === 'undefined' || !this.hasOwnProperty("metadata") || !this.metadata) {
             this.metadata = {};
@@ -153,11 +186,21 @@ module.exports = class Configuration {
         return this;
     }
 
+    /**
+    * Document the URL for a website project.
+    * @param {string} root_url
+    * @returns {Configuration}
+    */
     rootURL(root_url) {
         this.root_url = root_url;
         return this;
     }
 
+    /**
+     * Declare JavaScript to add within the head tag of rendered pages.
+     * @param script
+     * @returns {Configuration}
+     */
     addHeaderJavaScript(script) {
         if (typeof this.scripts === 'undefined' || !this.hasOwnProperty("scripts") || !this.scripts) {
             this.scripts = {};
@@ -167,6 +210,11 @@ module.exports = class Configuration {
         return this;
     }
 
+    /**
+     * Declare JavaScript to add at the bottom of rendered pages.
+     * @param script
+     * @returns {Configuration}
+     */
     addFooterJavaScript(script) {
         if (typeof this.scripts === 'undefined' || !this.hasOwnProperty("scripts") || !this.scripts) {
             this.scripts = {};
@@ -176,6 +224,11 @@ module.exports = class Configuration {
         return this;
     }
 
+    /**
+     * Declare a CSS Stylesheet to add within the head tag of rendered pages.
+     * @param script
+     * @returns {Configuration}
+     */
     addStylesheet(css) {
         if (typeof this.scripts === 'undefined' || !this.hasOwnProperty("scripts") || !this.scripts) {
             this.scripts = {};
@@ -189,6 +242,9 @@ module.exports = class Configuration {
         this.cheerio = cheerio;
     }
 
+    /**
+     * Copy the contents of all directories in assetDirs to the render destination.
+     */
     copyAssets() {
         log('copyAssets START');
 
@@ -212,6 +268,9 @@ module.exports = class Configuration {
         }));
     }
 
+    /**
+     * Call the onSiteRendered function of any plugin which has that function.
+     */
     hookSiteRendered() {
         return new Promise((resolve, reject) => {
             async.eachSeries(this._plugins,
@@ -232,6 +291,8 @@ module.exports = class Configuration {
     /**
      * use - go through plugins array, adding each to the plugins array in
      * the config file, then calling the config function of each plugin.
+     * @param PluginObj The plugin name or object to add
+     * @returns {Configuration}
      */
     use(PluginObj) {
         // console.log("Configuration #1 use PluginObj "+ typeof PluginObj +" "+ util.inspect(PluginObj));
@@ -261,7 +322,9 @@ module.exports = class Configuration {
     }
 
     /**
-     * plugin - Look for a plugin, returning its module reference.
+     * Look for a plugin, returning its module reference.
+     * @param {string} name
+     * @returns {Plugin}
      */
     plugin(name) {
         if (! this._plugins) {
