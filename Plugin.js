1d50549c697a7119b29964d72111639583736878
David Herron <david@davidherron.com>
2017-03-25 22:53:57 -0700

    Convert to Symbol and getter

--- Plugin.js
+++ Plugin.js
@@ -1,21 +1,23 @@
 'use strict';
 
+const _plugin_name = Symbol('name');
+
 module.exports = class Plugin {
-    
+
     constructor(name) {
-        this._name  = name;
+        this[_plugin_name]  = name;
     }
-    
+
     /**
      * Add this plugin to the configuration object.
      */
     configure(config) {
         throw new Error("Must implement configure function");
     }
-    
+
     /**
      * Getter for the plugin name
      */
-    get name() { return this._name; }
-    
+    get name() { return this[_plugin_name]; }
+
 };
