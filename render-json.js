314dcfcb07715d35608a0d3d4dea0b1f7787f256
David Herron <david@davidherron.com>
2017-03-25 22:57:54 -0700

    Cleanup

--- render-json.js
+++ render-json.js
@@ -19,7 +19,6 @@ class JSONRenderer extends HTMLRenderer {
     renderSync(text, metadata) {
         var json = JSON.parse(text);
         return mahaPartial.doPartialSync(metadata.JSONFormatter, { data: json });
-        // return akasha.partialSync(config, metadata.JSONFormatter, { data: json });
     }
 
     render(text, metadata) {
@@ -28,34 +27,13 @@ class JSONRenderer extends HTMLRenderer {
             // console.log(`JSONRenderer ${text} ==> ${util.inspect(json)}`);
             // console.log(`JSONRenderer JSONFormatter ${metadata.JSONFormatter}`);
             return yield mahaPartial.doPartialAsync(metadata.JSONFormatter, { data: json });
-            // return yield akasha.partial(config, metadata.JSONFormatter, { data: json });
         })
         .catch(e => {
             var docpath = metadata.document ? metadata.document.path : "unknown";
             var errstack = e.stack ? e.stack : e;
             throw "Error with JSON in file "+ docpath +" "+ errstack;
         });
-        /* return new Promise((resolve, reject) => {
-            try {
-                var json = JSON.parse(text);
-                resolve(akasha.partial(metadata.JSONFormatter, { data: json }));
-            } catch(e) {
-                var docpath = metadata.document ? metadata.document.path : "unknown";
-                var errstack = e.stack ? e.stack : e;
-                reject("Error with EJS in file "+ docpath +" "+ errstack);
-            }
-        }); */
     }
-
-    /**
-     * We cannot allow PHP code to run through Mahabhuta.
-     * /
-    doMahabhuta(fpath) {
-        if (/\.php\.ejs$/.test(fpath))
-            return false;
-        else
-            return true;
-    } */
 }
 
 module.exports = new JSONRenderer();
