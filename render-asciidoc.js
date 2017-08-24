1168f47f829a952cf9c9d49e361ed91f652ab12a
David Herron <david@davidherron.com>
2017-08-23 22:25:32 -0700

    Set up minimalized attributes for AsciiDoctor from metadata

--- render-asciidoc.js
+++ render-asciidoc.js
@@ -35,7 +35,24 @@ class AsciidocRenderer extends HTMLRenderer {
         var options = metadata.asciidoctor ? metadata.asciidoctor : {
             doctype: this[_renderer_doctype]
         };
-        // options.attributes = metadata;
+        // AsciiDoctor.js doesn't allow non-String/Number items in 
+        // the attributes object.  That means we cannot simply use
+        // the metadata as-is, but have to select out the items to include.
+        //
+        // First, this steps through the keys in metadata object looking
+        // for the items that are strings or numbers.  That limits the
+        // data items to what AsciiDoctor supports.
+        //
+        // Second, we skip the 'title' item because AsciiDoctor has
+        // special treatment for that attribute.
+        options.attributes = {};
+        for (let key in metadata) {
+            if ((typeof metadata[key] === 'string'
+              || typeof metadata[key] === 'number')
+              && key !== 'title') {
+                options.attributes[key] = metadata[key];
+            }
+        }
         // console.log(`convert ${util.inspect(options)}`);
         return asciidoctor.convert(text, options);
     }
