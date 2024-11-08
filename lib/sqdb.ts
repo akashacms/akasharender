a2f735d91123c0074b2b110c80c5dddabfdd0398
David Herron <david@davidherron.com>
2024-11-08 09:40:10 +0200

    SQL database documentation

--- lib/sqdb.ts
+++ lib/sqdb.ts
@@ -1,4 +1,11 @@
 
+/**
+ * SQL Database support using SQLITE3.
+ * 
+ * What's supported is SQLITE3ORM - a lightweight
+ * ORM that runs on top of SQLITE3.
+ */
+
 import { SqlDatabase } from 'sqlite3orm';
 
 export const sqdb = new SqlDatabase();;
